5b74b3114f49393382a85d3a2fabef7d518b1acc
Cristopher Ayala Paz <crisayala16@gmail.com>
2017-05-19 14:14:23 -0400

    update

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var express = require('express');
-var port = process.env.PORT || 8080;
-var app = express();
-
-var express = require('express');
+var exphbs = require("express-handlebars");
+var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var path = require('path');
 
 
 var app = express();
 var port = process.env.PORT || 8080;
-app.use(express.static(path.join(__dirname + '/public')));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -19,10 +18,10 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({
 	type: "application/vnd.api+json"
 }));
-
+app.use(express.static(path.join(__dirname + '/public')));
 app.use(require('./routes/htmlRoutes'));
 app.use(require('./routes/userRoutes'));
-app.use(require('./routes/productsRoutes'));
+app.use(require('./routes/productRoutes'));
 
 app.listen(port, function(err){
 	if(err){
